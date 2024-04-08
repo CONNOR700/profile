d93d5055ff3874d5b8b07777bc9676379f03f835
CONNOR700 <87907892+CONNOR700@users.noreply.github.com>
2024-04-08 14:59:56 +0700

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,12 @@
+function weChat() {
+    alert("wxid_kla05drizese22");
+}
+function discord() {
+    alert("connornaik")
+}
+function epicgames() {
+    alert("ActingTuna1997")
+}
+function xbox() {
+    alert("CONNOR701")
+}
